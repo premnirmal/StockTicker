939da5a9f4a9bf4c1e3a88fbfac74a9d3b350759
Prem Nirmal <prem@fuzzproductions.com>
2015-02-20 11:10:26 -0500

    added crashlytics

--- app/src/main/java/com/github/premnirmal/ticker/StocksApp.java
+++ app/src/main/java/com/github/premnirmal/ticker/StocksApp.java
@@ -2,8 +2,10 @@ package com.github.premnirmal.ticker;
 
 import android.app.Application;
 
+import com.crashlytics.android.Crashlytics;
 import com.github.premnirmal.tickerwidget.R;
 
+import io.fabric.sdk.android.Fabric;
 import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
 
 /**
@@ -14,6 +16,7 @@ public class StocksApp extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        Fabric.with(this, new Crashlytics());
         Injector.init(this);
         CalligraphyConfig.initDefault(new CalligraphyConfig.Builder()
                         .setDefaultFontPath("fonts/Montserrat-Regular.otf")
