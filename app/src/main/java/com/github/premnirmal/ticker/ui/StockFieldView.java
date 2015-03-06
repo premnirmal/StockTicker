ca7f174279485d100676c77656778c4f8ad582f6
Prem Nirmal <prem.nirmal88@gmail.com>
2015-03-05 23:48:16 -0500

    - Added widgets initial layout - Moved portfolio to its own fragment, and redesigned the view

--- app/src/main/java/com/github/premnirmal/ticker/ui/StockFieldView.java
+++ app/src/main/java/com/github/premnirmal/ticker/ui/StockFieldView.java
@@ -4,8 +4,10 @@ import android.content.Context;
 import android.content.res.TypedArray;
 import android.util.AttributeSet;
 import android.util.TypedValue;
+import android.view.Gravity;
 import android.view.LayoutInflater;
-import android.widget.RelativeLayout;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.github.premnirmal.tickerwidget.R;
@@ -13,7 +15,7 @@ import com.github.premnirmal.tickerwidget.R;
 /**
  * Created by premnirmal on 3/4/15.
  */
-public class StockFieldView extends RelativeLayout {
+public class StockFieldView extends LinearLayout {
 
     private TextView fieldName;
     private TextView fieldValue;
@@ -24,24 +26,30 @@ public class StockFieldView extends RelativeLayout {
 
     public StockFieldView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        LayoutInflater.from(context).inflate(R.layout.stock_field_view, this, true);
         fieldName = ((TextView) findViewById(R.id.fieldname));
         fieldValue = ((TextView) findViewById(R.id.fieldvalue));
         if (attrs != null) {
             final TypedArray array = getContext().obtainStyledAttributes(attrs, R.styleable.StockFieldView);
             final int orientation = array.getInt(R.styleable.StockFieldView_or, 0);
             if (orientation == 0) {
-                LayoutInflater.from(context).inflate(R.layout.stock_field_view, this, true);
+                setOrientation(HORIZONTAL);
+                fieldName.setLayoutParams(new LayoutParams(0, ViewGroup.LayoutParams.WRAP_CONTENT, 0.7f));
+                fieldValue.setLayoutParams(new LayoutParams(0, ViewGroup.LayoutParams.WRAP_CONTENT, 0.3f));
+                fieldValue.setGravity(Gravity.RIGHT);
             } else {
-                LayoutInflater.from(context).inflate(R.layout.stock_field_view_vertical, this, true);
+                setOrientation(VERTICAL);
+                fieldName.setLayoutParams(new LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, 0, 0.5f));
+                fieldValue.setLayoutParams(new LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, 0, 0.5f));
+                fieldValue.setGravity(Gravity.LEFT);
             }
+            setWeightSum(1f);
             final String name = getStringValue(context, array, R.styleable.StockFieldView_name);
             fieldName.setText(name);
             final float textSize = array.getDimensionPixelSize(R.styleable.StockFieldView_size, 20);
             fieldName.setTextSize(TypedValue.COMPLEX_UNIT_PX, textSize);
             fieldValue.setTextSize(TypedValue.COMPLEX_UNIT_PX, textSize * 0.8f);
             array.recycle();
-        } else {
-            LayoutInflater.from(context).inflate(R.layout.stock_field_view, this, true);
         }
     }
 
