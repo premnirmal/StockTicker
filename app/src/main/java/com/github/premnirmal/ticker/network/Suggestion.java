c68c5ae82ced4a86820ab595a901f44eb332404c
Prem Nirmal <prem@dwnld.me>
2015-12-01 10:51:31 -0500

    fixed yahoo suggestions

--- app/src/main/java/com/github/premnirmal/ticker/network/Suggestion.java
+++ app/src/main/java/com/github/premnirmal/ticker/network/Suggestion.java
@@ -1,19 +1,22 @@
 package com.github.premnirmal.ticker.network;
 
-import com.google.gson.annotations.SerializedName;
-
 /**
  * Created by premnirmal on 12/21/14.
  */
 public class Suggestion {
 
-    @SerializedName("Symbol")
+//    symbol: "AAPL",
+//    name: "Apple Inc.",
+//    exch: "NMS",
+//    type: "S",
+//    exchDisp: "NASDAQ",
+//    typeDisp: "Equity"
+
     public String symbol;
-
-    @SerializedName("Name")
     public String name;
-
-    @SerializedName("Exchange")
     public String exch;
+    public String type;
+    public String exchDisp;
+    public String typeDisp;
 
 }
