b1f4e7d4c52b16cb88f9cf671ca40b3067209a48
Prem Nirmal <prem.nirmal88@gmail.com>
2017-05-11 22:01:22 -0400

    circle fix, tests will fail now

--- test.sh
+++ test.sh
@@ -1,5 +1,8 @@
-./gradlew --stop
-
+#! /bin/bash
+code=0
 ./gradlew testPureflossDebug -PdisablePreDex --no-daemon
-
+if [ $? != 0 ] ; then
+  code=1
+fi
 cp -r app/build/reports/tests/ $CIRCLE_ARTIFACTS
+exit $code
