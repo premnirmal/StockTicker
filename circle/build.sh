fcea28c68f415de3e0cc6b3f2a77a6595d3c4e19
Prem Nirmal <prem.nirmal88@gmail.com>
2018-08-14 18:22:29 -0400

    circle2.0 attempt

--- circle/build.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-./gradlew assemblePureflossDebug -PdisablePreDex
-if [ $? != 0 ] ; then
-  exit 1
-fi
-cp -r app/build/outputs $CIRCLE_ARTIFACTS
--- circle/test.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-code=0
-./gradlew testPureflossDebug -PdisablePreDex --no-daemon
-if [ $? != 0 ] ; then
-  code=1
-fi
-cp -r app/build/reports/tests/ $CIRCLE_TEST_REPORTS/junit/
-cp -r app/build/test-results/testPureflossDebugUnitTest/ $CIRCLE_TEST_REPORTS/junit/
-exit $code
