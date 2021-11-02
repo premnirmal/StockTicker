709ed777c7f084ab67efee04d13225124add9285
Prem Nirmal <prem.nirmal88@gmail.com>
2021-11-02 10:56:40 +0000

    Use robolectric 4.3.1 to fix unit tests

--- app/src/test/kotlin/com/github/premnirmal/ticker/BaseUnitTest.kt
+++ app/src/test/kotlin/com/github/premnirmal/ticker/BaseUnitTest.kt
@@ -1,12 +1,10 @@
 package com.github.premnirmal.ticker
 
-import com.github.premnirmal.ticker.base.BaseFragment
 import com.github.premnirmal.ticker.mock.Mocker
 import com.github.premnirmal.ticker.model.FetchResult
 import com.github.premnirmal.ticker.model.IStocksProvider
 import com.github.premnirmal.ticker.model.IStocksProvider.FetchState
 import com.github.premnirmal.ticker.tools.Parser
-import com.github.premnirmal.tickerwidget.R
 import com.google.gson.JsonElement
 import com.nhaarman.mockitokotlin2.any
 import com.nhaarman.mockitokotlin2.doNothing
@@ -15,9 +13,7 @@ import junit.framework.TestCase
 import kotlinx.coroutines.test.runBlockingTest
 import org.junit.Before
 import org.junit.runner.RunWith
-import org.robolectric.Robolectric
 import org.robolectric.RobolectricTestRunner
-import org.robolectric.android.controller.ActivityController
 import java.lang.reflect.Type
 
 /**
@@ -25,24 +21,6 @@ import java.lang.reflect.Type
  */
 @RunWith(RobolectricTestRunner::class)
 abstract class BaseUnitTest : TestCase() {
-
-  companion object {
-
-    /**
-     * Attach a fragment to a new instance of {@link TestActivity.java}
-     *
-     * @param fragment Fragment to add to the activity.
-     */
-    fun attachFragmentToTestActivity(fragment: BaseFragment): ActivityController<TestActivity> {
-      val controller = Robolectric.buildActivity(TestActivity::class.java).create()
-      controller.start()
-      controller.resume()
-      val activity = controller.get()
-      val fm = activity.supportFragmentManager
-      fm.beginTransaction().add(R.id.fragment_container, fragment).commit()
-      return controller
-    }
-  }
 
   private val parser = Parser()
 
