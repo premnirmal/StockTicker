3e4296d77cc2006b46368c20da893e0d90614038
Prem Nirmal <prem.nirmal88@gmail.com>
2023-05-22 23:17:55 +0000

    Prevent ConcurrentModificationException for cookies map

--- app/src/main/kotlin/com/github/premnirmal/ticker/network/YahooFinanceCookies.kt
+++ app/src/main/kotlin/com/github/premnirmal/ticker/network/YahooFinanceCookies.kt
@@ -3,13 +3,14 @@ package com.github.premnirmal.ticker.network
 import okhttp3.Cookie
 import okhttp3.CookieJar
 import okhttp3.HttpUrl
+import java.util.concurrent.ConcurrentHashMap
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
 class YahooFinanceCookies @Inject constructor() : CookieJar {
 
-  private var _cookies = HashMap<String, Cookie>()
+  private var _cookies = ConcurrentHashMap<String, Cookie>()
 
   override fun saveFromResponse(url: HttpUrl, cookies: List<Cookie>) {
     cookies.forEach { cookie ->
