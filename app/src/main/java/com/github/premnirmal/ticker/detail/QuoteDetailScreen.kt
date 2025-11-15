eee8c154c1d591e1f57c195392a78faaec6ae6cf
Prem Nirmal <prnirmal@microsoft.com>
2025-11-15 08:17:08 +0000

    FAB with navigation bar window insets

--- app/src/main/java/com/github/premnirmal/ticker/detail/QuoteDetailScreen.kt
+++ app/src/main/java/com/github/premnirmal/ticker/detail/QuoteDetailScreen.kt
@@ -23,8 +23,10 @@ import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.lazy.grid.GridCells.Adaptive
 import androidx.compose.foundation.lazy.grid.GridCells.Fixed
 import androidx.compose.foundation.lazy.grid.GridItemSpan
@@ -192,6 +194,7 @@ private fun QuoteDetailContent(
         },
         floatingActionButton = {
             FloatingActionButton(
+                modifier = Modifier.windowInsetsPadding(WindowInsets.navigationBars),
                 onClick = {
                     showAddRemoveDialog = true
                 },
